b22d977527ee35d2b062263d67168f18257f4232
Souravmishra5116 <135313867+Souravmishra5116@users.noreply.github.com>
2025-03-09 20:07:13 +0530

    api setting

--- src/server.js
+++ src/server.js
@@ -9,6 +9,11 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/contact", contactRoutes);
+app.get("/api/test", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, message: "backend test api working perfectly" });
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
